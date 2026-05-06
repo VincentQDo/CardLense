b467234fe6b8931578ec32fe77d18e170b2b72e1
VinceQDo <vincentquangdo@gmail.com>
2026-05-06 11:07:46 +0000

    Update lint and prettier rules.

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,41 +4,74 @@ import tseslint from 'typescript-eslint';
 import json from '@eslint/json';
 import markdown from '@eslint/markdown';
 import css from '@eslint/css';
+import svelte from 'eslint-plugin-svelte';
+import prettier from 'eslint-config-prettier';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,svelte}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
   },
-  tseslint.configs.recommended,
+
+  js.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  ...svelte.configs['flat/recommended'],
+
+  {
+    files: ['**/*.{ts,svelte}'],
+    rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'error',
+      eqeqeq: ['error', 'always'],
+      curly: ['error', 'all'],
+      'no-implicit-coercion': 'error',
+
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+
+      'svelte/no-at-html-tags': 'warn',
+      'svelte/require-store-reactive-access': 'error'
+    }
+  },
+
   {
     files: ['**/*.json'],
     plugins: { json },
     language: 'json/json',
-    extends: ['json/recommended'],
+    extends: ['json/recommended']
   },
+
   {
     files: ['**/*.jsonc'],
     plugins: { json },
     language: 'json/jsonc',
-    extends: ['json/recommended'],
+    extends: ['json/recommended']
   },
+
   {
     files: ['**/*.md'],
     plugins: { markdown },
     language: 'markdown/gfm',
-    extends: ['markdown/recommended'],
+    extends: ['markdown/recommended']
   },
+
   {
     files: ['**/*.css'],
     plugins: { css },
     language: 'css/css',
     extends: ['css/recommended'],
     rules: {
-      'css/no-invalid-at-rules': 'off',
-    },
+      'css/no-invalid-at-rules': 'off'
+    }
   },
+
+  prettier
 ]);
